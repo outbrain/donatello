07fd3e5baa3f87843bef0f1920c636752736bd07
Roy Shell <rshell@obil-rshell.il.outbrain.com>
2017-01-15 15:29:21 +0200

    Added routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -9,5 +9,68 @@ export class Routes {
           title: 'Express Login'
         });
       });
+
+    //Ports API
+    app.route('/api/ports')
+      .post((req, res) => {
+        res.status(200).send();
+      });
+
+    app.route('/api/ports/:id')
+      .put((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports/:id')
+      .delete((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports:id?')
+      .get((req, res) => {
+		res.status(200).send();
+      });
+
+    //Route API
+    app.route('/api/ports/:portId/routes/:id')
+      .post((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports/:portId/routes/:id')
+      .put((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports/:portId/routes/:id')
+      .delete((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports/:portId/routes/:id')
+      .get((req, res) => {
+		res.status(200).send();
+      });
+
+    //Response API
+    app.route('/api/ports/:portId/routes/:routeId/responses/:id')
+      .post((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports/:portId/routes/:routeId/responses/:id')
+      .put((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports/:portId/routes/:routeId/responses/:id')
+      .delete((req, res) => {
+		res.status(200).send();
+      });
+
+    app.route('/api/ports/:portId/routes/:routeId/responses/:id')
+      .get((req, res) => {
+		res.status(200).send();
+      });
   }
 }
