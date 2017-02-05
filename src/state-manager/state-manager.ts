0654c59acae9924d50d15afe9d58a3f3beb4f43e
Vlad Ioffe <vlio20@gmail.com>
2017-02-05 10:25:41 +0200

    create stateManager singleton

--- src/state-manager/state-manager.ts
+++ src/state-manager/state-manager.ts
@@ -1,3 +1,8 @@
 export class StateManager {
+  private static
+  instance: StateManager;
 
+  public static get Instance() {
+    return StateManager.instance || (StateManager.instance = new StateManager());
+  }
 }
