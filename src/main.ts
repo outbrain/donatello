a2b65b265169dbc1211d8c38132b1da363d1bfba
Vlad Ioffe <vlio20@gmail.com>
2017-03-05 16:12:24 +0200

    adding interfaces to be exported

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
+export * from './state-manager/state.model';
+export * from './state-manager/port.model';
+export * from './state-manager/route.model';
+export * from './state-manager/response.model';
 export * from './services/state/state-service';
